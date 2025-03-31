1c6b36b1f76b58144a555019d4d7d7b8ecccac85
AbdelrahmanMohamed33 <144160951+AbdelrahmanMohamed33@users.noreply.github.com>
2025-04-01 01:18:54 +0200

    Update Clinicdetails.jsx

--- src/Components/Clinicdetails.jsx
+++ src/Components/Clinicdetails.jsx
@@ -31,26 +31,34 @@ const Clinicdetails = () => {
   if (!doctor) return <div className="text-center text-red-500">Doctor not found.</div>;
 
   return (
-    <div className="p-6 max-w-2xl mx-auto">
-      <div className="text-center mt-16">
-        <img src={img} alt="Doctor" className="w-32 h-32 rounded-full mx-auto" />
-        <h1 className="text-2xl font-bold mt-4">Dr. {doctor.name}</h1>
-        <p className="text-gray-500">{doctor.specialization}</p>
+    <div className="p-6 max-w-3xl mx-auto  ">
+      <div className="flex items-center bg-white shadow-md rounded-lg p-6 mt-24 py-16" >
+        {/* ✅ الصورة على اليسار */}
+        <img src={img} alt="Doctor" className="w-40 h-40 rounded-lg object-cover" />
+  
+        <div className="ml-6">
+          <h1 className="text-2xl font-bold text-blue-900 ">Dr. {doctor.doctor.fullName}</h1>
+          <p className="text-gray-500 mb-4">{doctor.doctor.email}</p>
+          <p className="mt-2 mb-1"><strong className="text-blue-900 ">Clinic Name:</strong> {doctor.name}</p>
+          <p className="mb-1"><strong className="text-blue-900  ">Address:</strong> {doctor.location}</p>
+          <p className="mb-1">
+            <strong className="text-blue-900 mb-2">Availability:</strong>
+            <span className="text-green-500 mb-2"> Start</span> {doctor.start} ::
+            <span className="text-red-500 mb-2"> Close</span> {doctor.close}
+          </p>
+          <p><strong className="text-blue-900">Fees:</strong> ${doctor.fees}</p>
+        </div>
       </div>
-
-      <div className="mt-6 p-16 border rounded-lg bg-white shadow-md ">
-        <p className="mb-2 "><strong className="text-blue-800 mr-2 ">Name:  </strong> {doctor.name}</p>
-        <p className="mb-2 "><strong className="text-blue-800 mr-2">Address:</strong> {doctor.location}</p>
-        <p className="mb-2 "><strong className="text-blue-800 mr-2">Availability:</strong> <span className="text-green-500">Start</span> {doctor.start} :: <span className="text-red-500">Close</span> {doctor.close} </p>
-        <p className="mb-2 "><strong className="text-blue-800 mr-2">Fees:</strong> ${doctor.fees}</p>
+  
+      {/* زر الحجز */}
+      <div className="flex justify-center items-center mt-6">
+        <button className="bg-blue-900 text-white px-5 py-2 rounded-lg hover:bg-blue-800">
+          Book Appointment
+        </button>
       </div>
-      <div className="flex justify-center items-center mt-4"><button className="  bg-blue-800 text-white px-5 py-2 rounded-lg hover:bg-blue-900">
-        Book Appointment
-      </button>
-      </div>
-      
     </div>
   );
+  
 };
 
 export default Clinicdetails;
