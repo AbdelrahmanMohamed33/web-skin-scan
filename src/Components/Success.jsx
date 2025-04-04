08723ac055dd13af12a92bafaa766b1279523ba7
AbdelrahmanMohamed33 <144160951+AbdelrahmanMohamed33@users.noreply.github.com>
2025-04-04 15:46:46 +0200

    Update Success.jsx

--- src/Components/Success.jsx
+++ src/Components/Success.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Lottie from 'lottie-react'; 
-import SuccessI from '../assets/success.json';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Lottie from "lottie-react";
+import SuccessI from "../assets/success.json";
 
 const AppointmentBooked = ({ doctorId }) => {
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    navigate.replace('/appointments');
+    navigate("/"); 
   };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
-      <div className="flex-grow flex items-center justify-center">
-        <Lottie animationData={SuccessI} style={{ width: 1000, height: 1000 ,marginTop:-170}} />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center">
+        <Lottie animationData={SuccessI} className="w-96 h-96" />
       </div>
-      <h1 className="text-2xl font-bold text-center mb-16 text-green-600 " style={{marginTop:-270}}>Successfully Booked</h1>
-      <div className="w-full max-w-md mx-auto">
+      <h1 className="text-2xl font-bold text-center text-green-600 mt-4">
+        Successfully Booked
+      </h1>
+      <div className="w-full max-w-md mx-auto mt-6">
         <button
           onClick={handleButtonClick}
-          className="w-full bg-blue-600 mb-16 hover:bg-blue-500 text-white font-bold py-2 rounded-lg transition duration-300"
+          className="w-full bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 rounded-lg transition duration-300"
         >
-          Your Appointment
+          Done
         </button>
       </div>
     </div>
   );
 };
+
 export default AppointmentBooked;
-
-
